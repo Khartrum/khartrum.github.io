710e29e4574b7dc22785d597c30fd60441c40076
adregan <duncanregan@gmail.com>
2013-07-22 14:08:20 +0900

    mobile animations

--- js/js.js
+++ js/js.js
@@ -1,1 +1,11 @@
-$( document ).ready();
+$( document ).ready(
+$('#mworkb').on("click", function() {
+	$("#mwork").slideToggle(200);
+})
+);
+
+$( document ).ready(
+$('#mblogb').on("click", function() {
+	$("#mblog").slideToggle(200);
+})
+);
